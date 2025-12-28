9531d2c9ce2ef5a9a1b699f078d568e86ad20073
maheswarRamisetty <106472608+maheswarRamisetty@users.noreply.github.com>
2025-12-28 09:42:02 +0530

    Remove DATA_PATH and add ALPHA constant
    Removed DATA_PATH variable and added ALPHA constant.

--- cfg.py
+++ cfg.py
@@ -4,8 +4,9 @@ ALPHA_S = 0.5
 ALPHA_N = 0.5   
 EMBED_BATCH_SIZE = 64
 NLI_BATCH_SIZE = 16
-DATA_PATH="F:/CognizantJudgeLLM/data/data.csv"
+DATA_PATH=--------
 TOKENIZER_NAME ="sentence-transformers/all-mpnet-base-v2"
 import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
 ALPHA = 0.5

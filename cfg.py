52a7797d42a2950392d66162330bbc6892b7a53d
maheswarRamisetty <maheshwarramisetty184@gmail.com>
2026-02-12 14:16:14 +0530

    Fix cfg

--- cfg.py
+++ cfg.py
@@ -5,7 +5,7 @@ ALPHA_N = 0.5
 EMBED_BATCH_SIZE = 64
 prompt = "Based on the above conversation, what is the decision regarding the customer's billing dispute?"
 NLI_BATCH_SIZE = 16
-DATA_PATH=r"C:\Users\RMSTVNMFST\mahesh\judge\data\three\datasummareis.csv"
+DATA_PATH=r"C:\Users\RMSTVNMFST\mahesh\judge\data\three\data_with_call_merged_three.csv"
 TOKENIZER_NAME ="sentence-transformers/all-mpnet-base-v2"
 import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
